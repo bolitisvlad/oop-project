fb52fe35e67ae9ba62182b2ef144125e6f2428e4
Bolitis Vlad <bolitisiani22@stud.ase.ro>
2024-01-05 23:22:30 +0200

    started implementing the menu

--- OOP_Project/OOP_Project/Source.cpp
+++ OOP_Project/OOP_Project/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include "Event.h"
 #include "Location.h"
@@ -7,5 +8,26 @@
 using namespace std;
 
 int main() {
-	
+	cout << "[1] Use file" << endl << "[2] Use CLI" << endl << "Enter your choise: ";
+	int answer;
+	cin >> answer;
+
+	string fileName;
+
+	if (answer == 1) {
+		cout << "Enter file name: ";
+		cin >> fileName;
+
+		ifstream file(fileName);
+
+		if (file.is_open()) {
+
+		}
+
+		file.close();
+	}
+
+	if (answer == 2) {
+
+	}
 }
